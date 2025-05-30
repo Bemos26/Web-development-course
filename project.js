b00b3fd0c2a81dc22d68dcd4634450f8866e497a
Benson <mosebemos26@gmail.com>
2025-05-30 11:41:24 +0300

    done with the project

--- project.js
+++ project.js
@@ -1,1 +1,10 @@
-console.log ('Hello world');
+// variables are used to store data that can change
+// constants just as the name suggests are used to store data that does not change
+// string are numbers and texts
+//boolean returns as true or false
+const themeToggleButton = document.getElementById("theme-toggle");
+const body = document.body;
+
+themeToggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+});
